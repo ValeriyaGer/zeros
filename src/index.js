869d9fe4743fec5c16288d1f7393e86713900f72
Gerasimenko Valeriya <gerasimenkovaleriya.i@gmail.com>
2018-09-27 13:35:16 +0200

    zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var j = 5, count = 0; 
+  var max = maxDegree(number);
+             
+  for (var ind = 0 ; ind < max; ind++){
+    count = count + div(number, j);
+    j = j*5;
+  };
+  return count;
+  
+  function maxDegree(a) {
+    var count = 0, i = 1;
+    while (i <= a) {
+      i = i*5;
+      count++;
+    };
+    return count;
+  };
+    
+  function div(a, b) {
+    return ( a - a % b)/b
+  };
+    
 }
